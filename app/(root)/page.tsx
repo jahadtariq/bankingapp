96a036419cfbec6e77db48b12b0f5b44f57883e7
jahadtariq <muhammadjahadt@gmail.com>
2024-05-11 02:17:46 +0500

    Started right sidebar on home page and created the cards component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
 
   // User to be fetched
-  const loggedIn = {firstName: 'Jahad'};
+  const loggedIn = {
+    firstName: 'Jahad',
+    lastName: 'Tariq',
+    email: 'muhammadjahadt@gmail.com'
+  };
 
   return (
     <section className='home'>
@@ -25,7 +30,18 @@ const Home = () => {
           />
           
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      {/* Now we add the right sidebar in our homepage since this sidebar is not avialable in anyother page except home that's why we will not include it in the layout */}
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 1234.50},{currentBalance: 5634.80}]}
+      />
+
     </section>
   )
 }
