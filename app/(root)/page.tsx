4ea93d575505601fb483def96c3864fae0dfdd5a
jahadtariq <muhammadjahadt@gmail.com>
2024-05-11 21:52:30 +0500

    Created the data on home page dynamic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,13 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Home = () => {
+const Home = async () => {
 
   // User to be fetched
-  const loggedIn = {
-    firstName: 'Jahad',
-    lastName: 'Tariq',
-    email: 'muhammadjahadt@gmail.com'
-  };
+  const loggedIn = await getLoggedInUser();
 
   return (
     <section className='home'>
@@ -19,7 +16,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
